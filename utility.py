c810a28af1d582a8c2b9f3cd5d430751d142d13a
yamei <1842850321@qq.com>
2020-03-16 11:20:29 +0800

    m

--- utility.py
+++ utility.py
@@ -4,7 +4,25 @@ import pyworld as pw
 import tensorflow as tf
 import os, shutil
 import glob
+def start_debugger_on_exception():
+    def info(type, value, tb):
+        if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+            # we are in interactive mode or we don't have a tty-like
+            # device, so we call the default hook
+            sys.__excepthook__(type, value, tb)
+        else:
+            import traceback, pdb
+            # we are NOT in interactive mode, print the exception..
+            traceback.print_exception(type, value, tb)
+            print()
+            # ...then start the debugger in post-mortem mode.
+            # pdb.pm() # deprecated
+            pdb.post_mortem(tb) # more "modern"
 
+    sys.excepthook = info
+
+
+start_debugger_on_exception()
 
 def get_speakers(trainset: str = './data/fourspeakers'):
     '''return current selected speakers for training
